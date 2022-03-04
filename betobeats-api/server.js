0469680ff513d1bbd6a27fc0ac4760cff4d2975d
Luiz Roberto Lethang Rodolpho <luiz@portaltelemedicina.com.br>
2022-03-03 22:57:33 -0300

    Include docker-compose. Include web project in the same solution. Update packages.

--- betobeats-api/server.js
+++ betobeats-api/server.js
@@ -1,33 +1,41 @@
-const restify = require('restify')
-const server = restify.createServer()
-const cors = require('cors')
-const port = 3006
-const beatport = require('./beatport')
-// const downloader = require('./downloader')
-// const tagparser = require('./tag-parser')
+var http = require('http');
 
-const initialize = async () => {
-    server.use(restify.plugins.acceptParser(server.acceptable))
-    server.use(restify.plugins.queryParser())
-    server.use(restify.plugins.bodyParser())
-    server.use(cors())
-    // server.use(auth.initialize())
-    // server.use(tokenParser)
-    // server.use(routeWall)
-    // server.opts('/*', acceptOpts)
-    // server.on('BadRequest', validationError)
-    // server.on('Unauthorized', validationError)
-    // server.on('Forbidden', validationError)
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(8080);
 
-    server.listen(port, () => console.info(`Server started @ ${port}`))
-    return server
-}
+// const restify = require('restify')
+// const server = restify.createServer()
+// const cors = require('cors')
+// const port = 3006
+// const beatport = require('./beatport')
+// // const downloader = require('./downloader')
+// // const tagparser = require('./tag-parser')
 
-const close = async () => {
-    return server.close()
-}
+// const initialize = async () => {
+//     // server.use(restify.plugins.acceptParser(server.acceptable))
+//     // server.use(restify.plugins.queryParser())
+//     // server.use(restify.plugins.bodyParser())
+//     // server.use(cors())
+//     // server.use(auth.initialize())
+//     // server.use(tokenParser)
+//     // server.use(routeWall)
+//     // server.opts('/*', acceptOpts)
+//     // server.on('BadRequest', validationError)
+//     // server.on('Unauthorized', validationError)
+//     // server.on('Forbidden', validationError)
 
-module.exports = {
-    initialize,
-    close
-}
+//     server.listen(port, () => console.info(`Server started @ ${port}`))
+//     return server
+// }
+
+// const close = async () => {
+//     return server.close()
+// }
+
+// module.exports = {
+//     initialize,
+//     close
+// }
